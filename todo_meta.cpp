8bde41e37583450a8cfc0d6339d70323bd92878e
Yuki Inoue <inouetakahiroki@gmail.com>
2012-11-28 07:36:35 +0900

    todo_meta -> active_record -> cpplib/

--- todo_meta.cpp
+++ todo_meta.cpp
@@ -1,45 +1,60 @@
-#include "todo_meta.hpp"
+#include "active_record.hpp"
 
 
 /*
 CREATE TABLE todos (id integer primary key autoincrement not null, name varchar(255) not null, planned float default 0, finished int default 0, start datetime, end datetime, importance integer default 0, learned varchar(255), hook string, todo_id int, routine_id int, divided boolean);
 */
 
-TODO_DEFINE_TAG(id)
-TODO_DEFINE_TAG(name)
-TODO_DEFINE_TAG(planned)
-TODO_DEFINE_TAG(finished)
-TODO_DEFINE_TAG(start)
-TODO_DEFINE_TAG(end)
-TODO_DEFINE_TAG(importance)
-TODO_DEFINE_TAG(learned)
-TODO_DEFINE_TAG(hook)
-TODO_DEFINE_TAG(todo_id)
-TODO_DEFINE_TAG(routine_id)
-TODO_DEFINE_TAG(divided)
-TODO_DEFINE_TAG(todos)
+ACTIVE_RECORD_DEFINE_TAG(name)
+ACTIVE_RECORD_DEFINE_TAG(planned)
+ACTIVE_RECORD_DEFINE_TAG(finished)
+ACTIVE_RECORD_DEFINE_TAG(start)
+ACTIVE_RECORD_DEFINE_TAG(end)
+ACTIVE_RECORD_DEFINE_TAG(importance)
+ACTIVE_RECORD_DEFINE_TAG(learned)
+ACTIVE_RECORD_DEFINE_TAG(hook)
+ACTIVE_RECORD_DEFINE_TAG(todo_id)
+ACTIVE_RECORD_DEFINE_TAG(routine_id)
+ACTIVE_RECORD_DEFINE_TAG(divided)
+ACTIVE_RECORD_DEFINE_TAG(todos)
 
 typedef SQLRow<
-  todo_id_tag, int, SQLRow
-  <todo_name_tag, std::string, SQLRow
-   <todo_planned_tag, double, SQLRow
-    <todo_finished_tag, int, SQLRow
-     <todo_start_tag, boost::posix_time::ptime, SQLRow
-      <todo_end_tag, boost::posix_time::ptime, SQLRow
-       <todo_importance_tag, int, SQLRow
-	<todo_learned_tag, std::string, SQLRow
-	 <todo_hook_tag, std::string, SQLRow
-	  <todo_todo_id_tag, int, SQLRow
-	   <todo_routine_id_tag, int, SQLRow
-	    <todo_divided_tag, bool, NilClass> > > > > > > > > > >
+  id_tag, int, SQLRow
+  <name_tag, std::string, SQLRow
+   <planned_tag, double, SQLRow
+    <finished_tag, int, SQLRow
+     <start_tag, boost::posix_time::ptime, SQLRow
+      <end_tag, boost::posix_time::ptime, SQLRow
+       <importance_tag, int, SQLRow
+	<learned_tag, std::string, SQLRow
+	 <hook_tag, std::string, SQLRow
+	  <todo_id_tag, int, SQLRow
+	   <routine_id_tag, int, SQLRow
+	    <divided_tag, bool, NilClass> > > > > > > > > > >
   > todo_row;
 
-typedef SQLTable<todo_todos_tag, todo_row> todo_table;
+typedef SQLTable<todos_tag, todo_row> todo_table;
 typedef SQLRowObject<todo_table> todo;
 
 
+
+#include <boost/format.hpp>
 #include <boost/foreach.hpp>
 #define foreach BOOST_FOREACH
+
+inline double planned(const todo &t){
+  return get(t, planned_tag());
+}
+
+void dump(const todo &todo){
+  std::cout
+    << boost::format("%3d| %5.1f/%5.1f/%5.1f| ")
+    % get(todo, id_tag())
+    % 0.0 % 0.0 % planned(todo)
+    << get(todo, name_tag()) << std::endl;
+}
+
+
 
 
 int main(){ // for test dump
@@ -71,9 +86,8 @@ int main(){ // for test dump
 
   cout << "found: " << v.size() << endl;
 
-  foreach(const todo &t, v){
-    cout << get(t, todo_name_tag()) << ": end:: " << get(t, todo_end_tag()) << endl;
-  }
+  foreach(const todo &t, v)
+    dump(t);
 
   sqlite3_close(db);
 }
